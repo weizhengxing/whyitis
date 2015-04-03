20fff439a33306cb42011802c3fa80dc32f82fe5
weizhengxing <xingweizheng@outlook.com>
2015-04-03 16:07:39 +0800

    hehe

---
+++ Test/src/test.java
@@ -0,0 +1,19 @@
+
+public class test {
+	int b;
+	public test (int m){
+		 int b=m;
+	}
+	public static void main(String args[]){
+		int s=5,a;
+
+
+		if(s==0){
+			a=2;
+		}
+		else{
+			a=3;
+		}
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
